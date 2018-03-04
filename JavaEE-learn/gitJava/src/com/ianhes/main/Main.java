b1708035408c6b134d6bb3d80d7220a00d6f14ec
Luan Ianhes <luan25ianhes@gmail.com>
2018-03-04 02:15:19 -0300

    modificado para entender git

--- JavaEE-learn/gitJava/src/com/ianhes/main/Main.java
+++ JavaEE-learn/gitJava/src/com/ianhes/main/Main.java
@@ -4,7 +4,15 @@ public class Main {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hi, World !!");
+		
+		String text = "Hi, world !!", text2 = "Olá Mundo !!!";
+		String n = "br";
+		
+		
+		if(n != "br") {
+			System.out.println(text);
+		}
+		
 		
 	}
 	
